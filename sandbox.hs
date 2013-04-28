6294d0cb7c2f65cbc5608bd4f3d6e6650073e7f6
Junichi Ito <jit@sonicgarden.jp>
2013-04-29 07:49:32 +0900

    Use hspec

--- sandbox.hs
+++ sandbox.hs
@@ -8,19 +8,24 @@ import Data.Time.Calendar.WeekDate
 import qualified Data.Text as T
 
 import System.Locale (defaultTimeLocale)
+
+import Test.Hspec
  
 rjust :: Int -> String -> String
 rjust width s = replicate (width - length s) ' ' ++ s
 
 renderCalendar :: Integer -> Int -> String
 renderCalendar y m = do
-      let h1 = T.unpack $ T.center 21 ' ' $ T.pack $ formatTime defaultTimeLocale "%B %Y" $ fromGregorian y m 1
-      let h2 = " Su Mo Tu We Th Fr Sa"
-      let (_,_,wday) = toWeekDate $ fromGregorian y m 1
-      let offset = replicate wday "   "
-      let calendar = map (rjust 3) $ map show $ take (gregorianMonthLength y m) [1..]
-      let body = map concat $ L.chunksOf 7 $ concat [offset, calendar]
-      unlines $ concat [[h1, h2], body]
+  let h1 = T.unpack $ T.center 21 ' ' $ T.pack $ formatTime defaultTimeLocale "%B %Y" $ fromGregorian y m 1
+  let h2 = " Su Mo Tu We Th Fr Sa"
+  let (_,_,wday) = toWeekDate $ fromGregorian y m 1
+  let offset = replicate wday "   "
+  let calendar = map (rjust 3) $ map show $ take (gregorianMonthLength y m) [1..]
+  let body = map concat $ L.chunksOf 7 $ concat [offset, calendar]
+  unlines $ concat [[h1, h2], body]
 
-main = do
-      putStr $ renderCalendar 2013 4
+main :: IO ()
+main = hspec $ do
+  describe "renderCalendar" $ do
+    it "returns calendar" $ do
+      renderCalendar 2013 4 `shouldBe` "      April 2013     \n Su Mo Tu We Th Fr Sa\n     1  2  3  4  5  6\n  7  8  9 10 11 12 13\n 14 15 16 17 18 19 20\n 21 22 23 24 25 26 27\n 28 29 30\n"
