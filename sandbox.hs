91376166f160c7efbeb243b57e1dded19ce48d1e
Junichi Ito <jit@sonicgarden.jp>
2013-04-29 07:27:46 +0900

    Extract function

--- sandbox.hs
+++ sandbox.hs
@@ -12,10 +12,15 @@ import System.Locale (defaultTimeLocale)
 rjust :: Int -> String -> String
 rjust width s = replicate (width - length s) ' ' ++ s
 
-main = do
-      putStrLn $ T.unpack $ T.center 21 ' ' $ T.pack $ formatTime defaultTimeLocale "%B %Y" $ fromGregorian 2013 4 1
-      putStrLn " Su Mo Tu We Th Fr Sa"
-      let (_,_,wday) = toWeekDate $ fromGregorian 2013 4 1
+renderCalendar :: Integer -> Int -> String
+renderCalendar y m = do
+      let h1 = T.unpack $ T.center 21 ' ' $ T.pack $ formatTime defaultTimeLocale "%B %Y" $ fromGregorian y m 1
+      let h2 = " Su Mo Tu We Th Fr Sa"
+      let (_,_,wday) = toWeekDate $ fromGregorian y m 1
       let offset = take wday $ repeat "   "
-      let calendar = map (rjust 3) $ map show $ take (gregorianMonthLength 2013 4) [1..]
-      putStr $ unlines $ map concat $ L.chunksOf 7 $ concat [offset, calendar]
+      let calendar = map (rjust 3) $ map show $ take (gregorianMonthLength y m) [1..]
+      let body = map concat $ L.chunksOf 7 $ concat [offset, calendar]
+      unlines $ concat [[h1, h2], body]
+
+main = do
+      putStr $ renderCalendar 2013 4
