e930eec1bb85ff20d67a0350a58d1584892df18a
Junichi Ito <jit@sonicgarden.jp>
2013-04-29 07:07:21 +0900

    Remove warning / Unused lines

--- sandbox.hs
+++ sandbox.hs
@@ -1,6 +1,6 @@
 import Data.Time.Clock
 import Data.Time.Calendar
-import Data.List.Split
+import qualified Data.List.Split as L
 
 import Data.Time
 import Data.Time.Calendar.WeekDate
@@ -9,10 +9,6 @@ import qualified Data.Text as T
 
 import System.Locale (defaultTimeLocale)
  
-
-date :: IO (Integer,Int,Int) -- :: (year,month,day)
-date = getCurrentTime >>= return . toGregorian . utctDay
-
 rjust :: Int -> String -> String
 rjust width s = replicate (width - length s) ' ' ++ s
 
@@ -21,51 +17,9 @@ myMap f (x:xs) = f x : myMap f xs
 myMap _ _      = []
 
 main = do
-      now <- getZonedTime
       putStrLn $ T.unpack $ T.center 21 ' ' $ T.pack $ formatTime defaultTimeLocale "%B %Y" $ fromGregorian 2013 4 1
       putStrLn " Su Mo Tu We Th Fr Sa"
       let (_,_,wday) = toWeekDate $ fromGregorian 2013 4 1
       let offset = take wday $ repeat "   "
       let calendar = map (rjust 3) $ map show $ take (gregorianMonthLength 2013 4) [1..]
-      putStr $ unlines $ myMap concat $ splitEvery 7 $ concat [offset, calendar]
-
-    -- putStrLn $ "Days in this month: " ++ (show $ gregorianMonthLength 2013 5)
--- import Data.Time
--- import System.Locale
--- 
--- dayAndTime :: ZonedTime -> (Day, TimeOfDay)
--- dayAndTime zt = let lt = zonedTimeToLocalTime zt
---                     day  = localDay lt
---                     time = localTimeOfDay lt
---                 in (day, time)
--- 
--- day  = fst . dayAndTime
--- time = snd . dayAndTime
--- 
--- diffTime :: TimeOfDay -> TimeOfDay -> TimeOfDay
--- diffTime t1 t2 = timeToTimeOfDay 
---                  (timeOfDayToTime t2 - timeOfDayToTime t1)
---                        
--- main = do 
---   -- Day 型の値の生成
---   print $ fromGregorian 2010 4 1
--- 
---   -- 2010.4.1 の 100 日後は？
---   print $ addDays 100 $ fromGregorian 2010 4 1
---   -- 2010.4.1 から大晦日までは何日？
---   print $ fromGregorian 2010 12 31 `diffDays` fromGregorian 2010 4 1
--- 
---   -- 現在の時刻を取得
---   now <- getZonedTime
--- 
---   -- 現在の日付から 100 日後は？
---   print $ addDays 100 (day now)
---   -- 現在から大晦日までは何日？
---   print $ fromGregorian 2010 12 31 `diffDays` (day now)
--- 
---   -- 後何分？
---   print $ diffTime (time now) (TimeOfDay 22 0 0) 
--- 
---   -- フォーマット
---   print $ formatTime defaultTimeLocale "%Y--%m--%d" 
---             $ fromGregorian 2010 4 1
+      putStr $ unlines $ myMap concat $ L.chunksOf 7 $ concat [offset, calendar]
