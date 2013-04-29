84a792802fbbe332b6b41a212bfd344170402bb2
Junichi Ito <jit@sonicgarden.jp>
2013-04-30 07:46:17 +0900

    Define applyTextFunction

--- CalendarRenderer.hs
+++ CalendarRenderer.hs
@@ -4,7 +4,7 @@ import qualified Data.List.Split as L (chunksOf)
 import Data.Time (formatTime, fromGregorian, gregorianMonthLength, toGregorian)
 import Data.Time.Calendar (Day)
 import Data.Time.Calendar.WeekDate (toWeekDate)
-import qualified Data.Text as T (pack, unpack, center, justifyRight)
+import qualified Data.Text as T (pack, unpack, center, justifyRight, Text)
 import System.Locale (defaultTimeLocale)
 
 dayLength :: Int
@@ -18,7 +18,7 @@ renderCalendar year month = do
 header :: Day -> [String]
 header day = do
   let weekLength = dayLength * 7
-  let center = T.unpack . T.center weekLength ' ' . T.pack
+  let center = applyTextFunction (T.center weekLength ' ')
   let monthYear = center $ formatTime defaultTimeLocale "%B %Y" day
   [monthYear, " Su Mo Tu We Th Fr Sa"]
 
@@ -28,11 +28,14 @@ body day = do
 
 calendar :: Day -> [String]
 calendar day = do
-  let format = T.unpack . T.justifyRight dayLength ' ' . T.pack . show
+  let rjust = applyTextFunction (T.justifyRight dayLength ' ') . show
   let (year,month,_) = toGregorian day
-  map format $ take (gregorianMonthLength year month) [1..]
+  map rjust $ take (gregorianMonthLength year month) [1..]
 
 firstWeekOffset :: Day -> [String]
 firstWeekOffset day = do
   let (_,_,wday) = toWeekDate day
   replicate wday $ replicate dayLength ' '
+
+applyTextFunction :: (T.Text -> T.Text) -> String -> String
+applyTextFunction f = T.unpack . f . T.pack
