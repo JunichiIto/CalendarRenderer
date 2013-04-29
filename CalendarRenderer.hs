212897c21d1b21fa2a5ee5c722c89ba57f8c2fb8
Junichi Ito <jit@sonicgarden.jp>
2013-04-30 06:28:13 +0900

    Change function order

--- CalendarRenderer.hs
+++ CalendarRenderer.hs
@@ -9,17 +9,17 @@ import System.Locale (defaultTimeLocale)
 
 dayLength :: Int
 dayLength = 3
+
+renderCalendar :: Integer -> Int -> String
+renderCalendar y m = do
+  let day = fromGregorian y m 1
+  unlines $ concat [header day, body day]
  
 header :: Day -> [String]
 header d = do
   let weekLength = dayLength * 7
   let monthYear = T.unpack $ T.center weekLength ' ' $ T.pack $ formatTime defaultTimeLocale "%B %Y" d
   [monthYear, " Su Mo Tu We Th Fr Sa"]
-
-firstWeekOffset :: Day -> [String]
-firstWeekOffset d = do
-  let (_,_,wday) = toWeekDate $ d
-  take wday $ repeat $ replicate dayLength ' '
 
 body :: Day -> [String]
 body d = do
@@ -28,7 +28,7 @@ body d = do
   let calendar = map (rjust dayLength) $ map show $ take (gregorianMonthLength y m) [1..]
   map concat $ L.chunksOf 7 $ concat [firstWeekOffset d, calendar]
 
-renderCalendar :: Integer -> Int -> String
-renderCalendar y m = do
-  let day = fromGregorian y m 1
-  unlines $ concat [header day, body day]
+firstWeekOffset :: Day -> [String]
+firstWeekOffset d = do
+  let (_,_,wday) = toWeekDate $ d
+  take wday $ repeat $ replicate dayLength ' '
