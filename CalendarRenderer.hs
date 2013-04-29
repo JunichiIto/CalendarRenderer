195001009e47a5ac4e92c2c2852b32488d37fe9d
Junichi Ito <jit@sonicgarden.jp>
2013-04-30 05:27:55 +0900

    Minimize static literal

--- CalendarRenderer.hs
+++ CalendarRenderer.hs
@@ -16,8 +16,7 @@ renderCalendar y m = do
   let monthYear = T.unpack $ T.center weekLength ' ' $ T.pack $ formatTime defaultTimeLocale "%B %Y" $ fromGregorian y m 1
   let header = [monthYear, " Su Mo Tu We Th Fr Sa"]
   let (_,_,wday) = toWeekDate $ fromGregorian y m 1
-  let offset = replicate wday "   "
+  let offset = take wday $ repeat $ replicate dayLength ' '
   let calendar = map (rjust dayLength) $ map show $ take (gregorianMonthLength y m) [1..]
   let body = map concat $ L.chunksOf 7 $ concat [offset, calendar]
   unlines $ concat [header, body]
-
