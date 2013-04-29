c3d550033f2d99b46f68a0a6ad9745d15d0bb441
Junichi Ito <jit@sonicgarden.jp>
2013-04-30 08:51:50 +0900

    Remove type declarations

--- CalendarRenderer.hs
+++ CalendarRenderer.hs
@@ -7,37 +7,29 @@ import Data.Time.Calendar.WeekDate (toWeekDate)
 import qualified Data.Text as T (pack, unpack, center, justifyRight, Text)
 import System.Locale (defaultTimeLocale)
 
-dayLength :: Int
 dayLength = 3
 
-weekLength :: Int
 weekLength = dayLength * 7
 
-renderCalendar :: Integer -> Int -> String
 renderCalendar year month = do
   let day = fromGregorian year month 1
   unlines $ header day ++ body day
  
-header :: Day -> [String]
 header day = do
   let center = applyTextFunction $ T.center weekLength ' '
   let monthYear = center $ formatTime defaultTimeLocale "%B %Y" day
   [monthYear, " Su Mo Tu We Th Fr Sa"]
 
-body :: Day -> [String]
 body day = map concat $ L.chunksOf 7 $ firstWeekOffset day ++ calendar day
 
-calendar :: Day -> [String]
 calendar day = do
   let rjust = applyTextFunction $ T.justifyRight dayLength ' '
   let (year, month, _) = toGregorian day
   map (rjust . show) [1..gregorianMonthLength year month]
 
-firstWeekOffset :: Day -> [String]
 firstWeekOffset day = do
   let (_, _, wday) = toWeekDate day
   let offsetLength = if wday == 7 then 0 else wday
   replicate offsetLength $ replicate dayLength ' '
 
-applyTextFunction :: (T.Text -> T.Text) -> String -> String
 applyTextFunction f = T.unpack . f . T.pack
