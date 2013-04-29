f08e8ed4a873455ace3f3c030f7bf53dd4d47f65
Junichi Ito <jit@sonicgarden.jp>
2013-04-30 06:23:29 +0900

    Extract constant

--- CalendarRenderer.hs
+++ CalendarRenderer.hs
@@ -6,29 +6,31 @@ import Data.Time.Calendar (Day)
 import Data.Time.Calendar.WeekDate (toWeekDate)
 import qualified Data.Text as T (pack, unpack, center)
 import System.Locale (defaultTimeLocale)
+
+dayLength :: Int
+dayLength = 3
  
 rjust :: Int -> String -> String
 rjust width s = replicate (width - length s) ' ' ++ s
 
-header :: Day -> Int -> [String]
-header d dayLength = do
+header :: Day -> [String]
+header d = do
   let weekLength = dayLength * 7
   let monthYear = T.unpack $ T.center weekLength ' ' $ T.pack $ formatTime defaultTimeLocale "%B %Y" d
   [monthYear, " Su Mo Tu We Th Fr Sa"]
 
-firstWeekOffset :: Day -> Int -> [String]
-firstWeekOffset d dayLength = do
+firstWeekOffset :: Day -> [String]
+firstWeekOffset d = do
   let (_,_,wday) = toWeekDate $ d
   take wday $ repeat $ replicate dayLength ' '
 
-body :: Day -> Int -> [String]
-body d dayLength = do
+body :: Day -> [String]
+body d = do
   let (y,m,_) = toGregorian d
   let calendar = map (rjust dayLength) $ map show $ take (gregorianMonthLength y m) [1..]
-  map concat $ L.chunksOf 7 $ concat [firstWeekOffset d dayLength, calendar]
+  map concat $ L.chunksOf 7 $ concat [firstWeekOffset d, calendar]
 
 renderCalendar :: Integer -> Int -> String
 renderCalendar y m = do
-  let dayLength = 3
   let day = fromGregorian y m 1
-  unlines $ concat [header day dayLength, body day dayLength]
+  unlines $ concat [header day, body day]
