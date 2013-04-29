880c423f342c85af71da6faa03958f3a6fcb116e
Junichi Ito <jit@sonicgarden.jp>
2013-04-30 06:30:37 +0900

    Get variables readable

--- CalendarRenderer.hs
+++ CalendarRenderer.hs
@@ -11,24 +11,24 @@ dayLength :: Int
 dayLength = 3
 
 renderCalendar :: Integer -> Int -> String
-renderCalendar y m = do
-  let day = fromGregorian y m 1
+renderCalendar year month = do
+  let day = fromGregorian year month 1
   unlines $ concat [header day, body day]
  
 header :: Day -> [String]
-header d = do
+header day = do
   let weekLength = dayLength * 7
-  let monthYear = T.unpack $ T.center weekLength ' ' $ T.pack $ formatTime defaultTimeLocale "%B %Y" d
+  let monthYear = T.unpack $ T.center weekLength ' ' $ T.pack $ formatTime defaultTimeLocale "%B %Y" day
   [monthYear, " Su Mo Tu We Th Fr Sa"]
 
 body :: Day -> [String]
-body d = do
-  let (y,m,_) = toGregorian d
+body day = do
+  let (year,month,_) = toGregorian day
   let rjust width s = replicate (width - length s) ' ' ++ s
-  let calendar = map (rjust dayLength) $ map show $ take (gregorianMonthLength y m) [1..]
-  map concat $ L.chunksOf 7 $ concat [firstWeekOffset d, calendar]
+  let calendar = map (rjust dayLength) $ map show $ take (gregorianMonthLength year month) [1..]
+  map concat $ L.chunksOf 7 $ concat [firstWeekOffset day, calendar]
 
 firstWeekOffset :: Day -> [String]
-firstWeekOffset d = do
-  let (_,_,wday) = toWeekDate $ d
+firstWeekOffset day = do
+  let (_,_,wday) = toWeekDate day
   take wday $ repeat $ replicate dayLength ' '
